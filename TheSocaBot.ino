939de42e21d66613e19ecf272b0be03007199f8f
Elwin Hammer <e.hammer@nazcasolutions.nl>
2019-11-27 09:20:15 +0100

    Update TheSocaBot.ino
    Eerste setup, header met 2 basis functies.
    V.1.0.0.

--- TheSocaBot.ino
+++ TheSocaBot.ino
@@ -1,1 +1,35 @@
-#
+/*
+#############################################################
+#Firmware:           TheSocaBot                             #
+#Hardware:           Arduino Mega 2560                      #
+#Eerste opzet:       26-11-2019                             #
+#Auteurs: E. Hammer | N. Vollebregt | M. Remmig | O. Cekem  #
+#Laatst gewijzigd:   27-11-2019                             #
+#Versie:             1.0.0                                  #
+#############################################################
+
+##WAT JE NIET MAG GEBRUIKEN##
+1.  Alle functies uit Servo.lib
+a.  attach()
+b.  write()
+c.  writeMicroseconds()
+d.  read()
+e.  attached()
+f.  detach()
+2.  Alle functies voor externe sensoren. Bijvoorbeeld:
+a.  De ping sensor
+b.  De Sharp afstand sensor
+c.  De accelerometrie sensoren
+3.  Eenvoudige functies voor bewerkingen
+a.  y = map(x, 1, 50, 50, 1);
+b.  Enz.
+*/
+
+
+void setup() {
+
+}
+
+void loop() {
+
+}
