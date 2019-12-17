2fc5d0bcc2e43124fc61153aaa93f3eb514345d0
mremmig <mremmig@users.noreply.github.com>
2019-12-17 17:50:20 +0100

    Update: meerdere keren uitlezen
    Functie leest nu 10 keer IR-sensoren uit met delay van 5ms ertussen en neemt het gemiddelde om zo de input te 'smoothen'

--- RnDfirmware/Uitlezen_IRsensoren/Uitlezen_IRsensoren.ino
+++ RnDfirmware/Uitlezen_IRsensoren/Uitlezen_IRsensoren.ino
@@ -1,12 +1,14 @@
 // Code voor het uitlezen van IR sensoren en aansturen van de bijbehorende leds
 // Plus opzet voor aansturing servo's
 
-const int IRpin_l = A5;   // Pins die door de functie gebruikt worden
-const int IRpin_m = A6;
-const int IRpin_r = A7;
+const int IRpin_l = A3;   // Pins die door de functie gebruikt worden
+const int IRpin_m = A4;
+const int IRpin_r = A5;
 const int LEDpin_l = 3;
 const int LEDpin_m = 4;
 const int LEDpin_r = 5;
+const int NumReadings = 10;
+const int CompareValue_IR = 900;
 
 char IRsensorRead(void);
 
@@ -15,6 +17,7 @@ void setup()  // Leds als OUTPUT instellen
   pinMode(LEDpin_l, OUTPUT);
   pinMode(LEDpin_m, OUTPUT);
   pinMode(LEDpin_r, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop()
@@ -25,30 +28,46 @@ void loop()
 }
 
 
-char IRsensorRead(void)  // leest IR sensoren twee keer uit met een delay ertussen en vergelijkt met grenswaarde
+char IRsensorRead(void)
 {
-   int IRvalue_l1 = 0;
-   int IRvalue_m1 = 0;
-   int IRvalue_r1 = 0;
-   int IRvalue_l2 = 0;
-   int IRvalue_m2 = 0;
-   int IRvalue_r3 = 0;
-   int CompareValue_IR = 300;
-   char ReturnValue_IR;
+  int IRvalue_l[NumReadings];   // een array voor elke IR sensor: (l)inks, (m)idden en (r)echts
+  int IRvalue_m[NumReadings];
+  int IRvalue_r[NumReadings];
+  
+  int tot_l = 0;   // totaal van array
+  int tot_m = 0;
+  int tot_r = 0;
+  
+  int avg_l = 0;   // gemiddelde van array
+  int avg_m = 0;
+  int avg_r = 0;
+  
+  char ReturnValue_IR;   // bitreeks waarvan de 3 LSB veranderen op basis van sensorwaarden
+  
+  int i;  // teller voor vullen van array
 
-   IRvalue_l1 = analogRead(IRpin_l);
-   IRvalue_m1 = analogRead(IRpin_m);
-   IRvalue_r1 = analogRead(IRpin_r);
-   delay(5);
-   IRvalue_l2 = analogRead(IRpin_l);
-   IRvalue_m2 = analogRead(IRpin_m);
-   IRvalue_r2 = analogRead(IRpin_r);
-
-   if(IRvalue_l1 < CompareValue_IR || IRvalue_l2 < CompareValue_IR) ReturnValue_IR |= 0x01; else ReturnValue_IR &= ~(0x01);
-   if(IRvalue_m1 < CompareValue_IR || IRvalue_m2 < CompareValue_IR) ReturnValue_IR |= 0x02; else ReturnValue_IR &= ~(0x02);
-   if(IRvalue_r1 < CompareValue_IR || IRvalue_r2 < CompareValue_IR) ReturnValue_IR |= 0x04; else ReturnValue_IR &= ~(0x04);
-
-   return ReturnValue_IR;
+  for (i = 0; i < NumReadings; i++)
+  {
+    IRvalue_l[i] = analogRead(IRpin_l);
+    tot_l = tot_l + IRvalue_l[i];
+    IRvalue_m[i] = analogRead(IRpin_m);
+    tot_m = tot_m + IRvalue_m[i];
+    IRvalue_r[i] = analogRead(IRpin_r);
+    tot_r = tot_r + IRvalue_r[i];
+    delay(5);
+  }
+  
+  avg_l = tot_l / NumReadings;
+  avg_m = tot_m / NumReadings;
+  avg_r = tot_r / NumReadings;
+  Serial.println(avg_m);
+  
+  
+  if(avg_l < CompareValue_IR) ReturnValue_IR |= 0x01; else ReturnValue_IR &= ~(0x01);
+  if(avg_m < CompareValue_IR) ReturnValue_IR |= 0x02; else ReturnValue_IR &= ~(0x02);
+  if(avg_r < CompareValue_IR) ReturnValue_IR |= 0x04; else ReturnValue_IR &= ~(0x04);
+  
+  return ReturnValue_IR;
 }
 
 //void IRcase(char w)
@@ -105,14 +124,14 @@ void IRcaseLED(char LEDs)
     case 0x01:
     {
       digitalWrite(LEDpin_l, HIGH);
-      Serial.println("links");
+      //Serial.println("links");
     }
     break;
   
     case 0x02:
     {
       digitalWrite(LEDpin_m, HIGH);
-      Serial.println("midden");
+      //Serial.println("midden");
     }
     break;
 
@@ -120,14 +139,14 @@ void IRcaseLED(char LEDs)
     {
       digitalWrite(LEDpin_l, HIGH);
       digitalWrite(LEDpin_m, HIGH);
-      Serial.println("linksmidden");
+      //Serial.println("linksmidden");
     }
     break;
   
     case 0x04:
     {
       digitalWrite(LEDpin_r, HIGH);\
-      Serial.println("rechts");
+      //Serial.println("rechts");
     }
     break;
 
@@ -135,7 +154,7 @@ void IRcaseLED(char LEDs)
     {
       digitalWrite(LEDpin_r, HIGH);
       digitalWrite(LEDpin_m, HIGH);
-      Serial.println("rechtsmidden");
+      //Serial.println("rechtsmidden");
     }
     break;
 
@@ -144,7 +163,7 @@ void IRcaseLED(char LEDs)
       digitalWrite(LEDpin_l, HIGH);
       digitalWrite(LEDpin_m, HIGH);
       digitalWrite(LEDpin_r, HIGH);
-      Serial.println("midden");
+      //Serial.println("midden");
     }
     break;
   }
