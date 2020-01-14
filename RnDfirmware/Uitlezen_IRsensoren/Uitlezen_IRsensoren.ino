8cfc2eb42f8e105a218f56fe6a6c1103e7d7b350
mremmig <mremmig@users.noreply.github.com>
2020-01-14 18:18:26 +0100

    Update Uitlezen_IRsensoren.ino
    No more DELAY!
    Kortere, simpelere functie zonder zonder averages. Servo library bijgevoegd voor testen, boebot volgt adh van deze code IR signalen.

--- RnDfirmware/Uitlezen_IRsensoren/Uitlezen_IRsensoren.ino
+++ RnDfirmware/Uitlezen_IRsensoren/Uitlezen_IRsensoren.ino
@@ -1,19 +1,36 @@
 // Code voor het uitlezen van IR sensoren en aansturen van de bijbehorende leds
 // Plus opzet voor aansturing servo's
 
-const int IRpin_l = A5;   // Pins die door de functie gebruikt worden
+// TEST SERVO AANTURING MET LIBRARY
+#include <Servo.h>
+Servo servoL;
+Servo servoR;
+void Servosturing(int, int);
+
+
+// Pins die door de functie gebruikt worden
+const int IRpin_l = A5; 
 const int IRpin_m = A6;
 const int IRpin_r = A7;
 const int LEDpin_l = 3;
 const int LEDpin_m = 4;
 const int LEDpin_r = 5;
-const int NumReadings = 4;
+// Grenswaarde IR sensoren (nog niet geoptimaliseerd)
 const int CompareValue_IR = 512;
 
+// Globale variabele om laatste uitlezeing te bewaren
+char ReturnValue_IR = 0;
+
+// Prototypes sensoren
 char IRsensorRead(void);
+void IRcase(char);
+void IRcaseLED(char);
+
 
 void setup()  // Leds als OUTPUT instellen
 {
+  servoL.attach(10);
+  servoR.attach(11);
   pinMode(LEDpin_l, OUTPUT);
   pinMode(LEDpin_m, OUTPUT);
   pinMode(LEDpin_r, OUTPUT);
@@ -23,96 +40,88 @@ void setup()  // Leds als OUTPUT instellen
 void loop()
 {
   char IRsensorOutput = IRsensorRead();
-  //IRcase(IRsensorOutput);
+  IRcase(IRsensorOutput);
   IRcaseLED(IRsensorOutput);
 }
 
-
 char IRsensorRead(void)
 {
-  int IRvalue_l[NumReadings];   // een array voor elke IR sensor: (l)inks, (m)idden en (r)echts
-  int IRvalue_m[NumReadings];
-  int IRvalue_r[NumReadings];
-  
-  int tot_l = 0;   // totaal van array
-  int tot_m = 0;
-  int tot_r = 0;
-  
-  int avg_l = 0;   // gemiddelde van array
-  int avg_m = 0;
-  int avg_r = 0;
-  
-  char ReturnValue_IR;   // bitreeks waarvan de 3 LSB veranderen op basis van sensorwaarden
-  
-  int i;  // teller voor vullen van array
+  int IRvalue_l = 0;
+  int IRvalue_m = 0;
+  int IRvalue_r = 0;
 
-  for (i = 0; i < NumReadings; i++)
+// Inverteert de waardes van de sensoren zodat detectie = hoge waarde
+  IRvalue_l = 1024 - analogRead(IRpin_l);
+  IRvalue_m = 1024 - analogRead(IRpin_m);
+  IRvalue_r = 1024 - analogRead(IRpin_r);
+//  DEBUG
+//  Serial.print(IRvalue_l);
+//  Serial.print("\t");
+//  Serial.print(IRvalue_m);
+//  Serial.print("\t");
+//  Serial.print(IRvalue_r);
+//  Serial.print("\n");
+ 
+
+  if(IRvalue_l > CompareValue_IR || IRvalue_m > CompareValue_IR || IRvalue_r > CompareValue_IR)
   {
-    IRvalue_l[i] = analogRead(IRpin_l);
-    tot_l = tot_l + IRvalue_l[i];
-    IRvalue_m[i] = analogRead(IRpin_m);
-    tot_m = tot_m + IRvalue_m[i];
-    IRvalue_r[i] = analogRead(IRpin_r);
-    tot_r = tot_r + IRvalue_r[i];
-    delay(13);
+    if(IRvalue_l > CompareValue_IR) ReturnValue_IR |= 0x01; else ReturnValue_IR &= ~(0x01);
+    if(IRvalue_m > CompareValue_IR) ReturnValue_IR |= 0x02; else ReturnValue_IR &= ~(0x02);
+    if(IRvalue_r > CompareValue_IR) ReturnValue_IR |= 0x04; else ReturnValue_IR &= ~(0x04);
   }
-  
-  avg_l = 2 * (1024 - tot_l / NumReadings);
-  avg_m = 2 * (1024 - tot_m / NumReadings);
-  avg_r = 2 * (1024 - tot_r / NumReadings);
-  Serial.println(avg_m);
-  
-  
-  if(avg_l > CompareValue_IR) ReturnValue_IR |= 0x01; else ReturnValue_IR &= ~(0x01);
-  if(avg_m > CompareValue_IR) ReturnValue_IR |= 0x02; else ReturnValue_IR &= ~(0x02);
-  if(avg_r > CompareValue_IR) ReturnValue_IR |= 0x04; else ReturnValue_IR &= ~(0x04);
-  
   return ReturnValue_IR;
 }
 
-//void IRcase(char w)
-//{
-//  switch(w)
-//  case 0x01:
-//  {
-//    //stuur scherp links
-//  }
-//  break;
-//
-//  case 0x02:
-//  {
-//    // rij rechtdoor
-//  }
-//  break;
-//  
-//  case 0x03:
-//  {
-//    // stuur langzaam links
-//  }
-//  break;
-//
-//  case 0x04:
-//  {
-//    // stuur scherp rechts
-//  }
-//  break;
-//
-//  case 0x06:
-//  {
-//    // stuur langzaam rechts
-//  }
-//  break;
-//  case 0x07:
-//  {
-//    // rechtdoor (alle sensoren HIGH)
-//  }
-//
-//  default:
-//  {
-//    // rondje draaien (leader kwijt)
-//  }
-//  
-//}
+void IRcase(char w)
+{
+  switch(w)
+  {
+    case 0x01:
+    {
+      Servosturing(1400,1400);
+      //stuur scherp links
+    }
+    break;
+  
+    case 0x02:
+    {
+      Servosturing(1400,1600);
+      // rij rechtdoor
+    }
+    break;
+    
+    case 0x03:
+    {
+      Servosturing(1400,1500);
+      // stuur langzaam links
+    }
+    break;
+  
+    case 0x04:
+    {
+      Servosturing(1600,1600);
+      // stuur scherp rechts
+    }
+    break;
+  
+    case 0x06:
+    {
+      Servosturing(1500,1600);
+      // stuur langzaam rechts
+    }
+    break;
+    case 0x07:
+    {
+      Servosturing(1400,1600);
+      // rechtdoor (alle sensoren HIGH)
+    }
+  
+    default:
+    {
+      // rondje draaien (leader kwijt)
+    }
+  }
+}
 
 void IRcaseLED(char LEDs)
 {
@@ -124,14 +133,14 @@ void IRcaseLED(char LEDs)
     case 0x01:
     {
       digitalWrite(LEDpin_l, HIGH);
-      //Serial.println("links");
+//      Serial.println("links");
     }
     break;
   
     case 0x02:
     {
       digitalWrite(LEDpin_m, HIGH);
-      //Serial.println("midden");
+//      Serial.println("midden");
     }
     break;
 
@@ -139,14 +148,14 @@ void IRcaseLED(char LEDs)
     {
       digitalWrite(LEDpin_l, HIGH);
       digitalWrite(LEDpin_m, HIGH);
-      //Serial.println("linksmidden");
+//      Serial.println("linksmidden");
     }
     break;
   
     case 0x04:
     {
       digitalWrite(LEDpin_r, HIGH);\
-      //Serial.println("rechts");
+//      Serial.println("rechts");
     }
     break;
 
@@ -154,7 +163,7 @@ void IRcaseLED(char LEDs)
     {
       digitalWrite(LEDpin_r, HIGH);
       digitalWrite(LEDpin_m, HIGH);
-      //Serial.println("rechtsmidden");
+//      Serial.println("rechtsmidden");
     }
     break;
 
@@ -163,8 +172,14 @@ void IRcaseLED(char LEDs)
       digitalWrite(LEDpin_l, HIGH);
       digitalWrite(LEDpin_m, HIGH);
       digitalWrite(LEDpin_r, HIGH);
-      //Serial.println("midden");
+//      Serial.println("midden");
     }
     break;
   }
+}
+
+void Servosturing(int Links, int Rechts)
+{       
+      servoL.writeMicroseconds(Links);
+      servoR.writeMicroseconds(Rechts);
 }
