1432c9a5a8abf80c5b6832a3c699f03aebfe4a74
OnurcanCekem <48911241+OnurcanCekem@users.noreply.github.com>
2020-01-27 21:19:56 +0100

    Fixing old servo drive function.
    I actually bothered to fix this. It only costs me 2 minutes. I want those 2 minutes back.

--- RnDfirmware/Aansturing_BoebotB/Aansturing_BoebotB.ino
+++ RnDfirmware/Aansturing_BoebotB/Aansturing_BoebotB.ino
@@ -8,7 +8,7 @@
 // pin 10 = right servo (inverted, 1400 = forward).
 
 int ServoLeft = 11;
-int servoRight = 10;
+int ServoRight = 10;
 
 void Drive (int timeL, int timeR); // Prototype for the Drive function for the Boe-Bot.
 void ServoStop(); // Prototype for the ServoStop function.
@@ -22,9 +22,9 @@ void ServoBackward(); // Prototype for the ServoBackward function.
 void setup() {
   Serial.begin(9600);
   
-  // Initialize ServoLeft and servoRight.
+  // Initialize ServoLeft and ServoRight.
   pinMode(ServoLeft, OUTPUT);
-  pinMode(servoRight, OUTPUT);
+  pinMode(ServoRight, OUTPUT);
 }
 
 void loop() {
@@ -37,47 +37,50 @@ void loop() {
 // Drive function for giving the servo's the correct time to turn on and off based on their given timers. This allows the vehicle to drive with no limits.
 // The values given should be between 1000-2000 in order to work properly.
 void Drive (int timeL, int timeR){
-  
+  int timer;
   // If the time is equal then there is no need to do timeL - timeR.
   if(timeL == timeR){
     digitalWrite(ServoLeft, HIGH);
-    digitalWrite(servoRight, HIGH);
+    digitalWrite(ServoRight, HIGH);
     delayMicroseconds(timeL);
-    digitalWrite(servoRight, LOW);
+    digitalWrite(ServoRight, LOW);
     digitalWrite(ServoLeft, LOW);
+    timer = timeL;
   }
   
   // If timeL is greater than timeR then turn servoR low earlier.
   else if(timeL > timeR){
     digitalWrite(ServoLeft, HIGH);
-    digitalWrite(servoRight, HIGH);
+    digitalWrite(ServoRight, HIGH);
     delayMicroseconds(timeR);
-    digitalWrite(servoRight, LOW);
+    digitalWrite(ServoRight, LOW);
     delayMicroseconds(timeL-timeR);
     digitalWrite(ServoLeft, LOW);
+    timer = timeL;
   }
 
-  // If timeR is greater than timeR then turn servoL low earlier.
-  else if(timeL < timeR){
+  // Else timeR is greater than timeR then turn servoL low earlier.
+  else{
     digitalWrite(ServoLeft, HIGH);
-    digitalWrite(servoRight, HIGH);
+    digitalWrite(ServoRight, HIGH);
     delayMicroseconds(timeL);
-    digitalWrite(servoLeft, LOW);
+    digitalWrite(ServoLeft, LOW);
     delayMicroseconds(timeR-timeL);
     digitalWrite(ServoRight, LOW);
+    timer = timeR;
   }
   
-  // Delay of 20 ms.
-  delay(20);
+  // Delay of 20 - timer ms. This is to ensure the 50 Hz.
+  delay(20-timer);
 }
 
 // Working basic function.
 //void Drive (int t){
 //  digitalWrite(ServoLeft, HIGH);
-//  digitalWrite(servoRight, HIGH);
+//  digitalWrite(ServoRight, HIGH);
 //  delayMicroseconds(t);
 //  digitalWrite(ServoLeft, LOW);
-//  digitalWrite(servoRight, LOW);
+//  digitalWrite(ServoRight, LOW);
 //  delay(20);
 //}
 
